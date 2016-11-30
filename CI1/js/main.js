26a585a01782a7a15fb81cb66af340600f184e84
huongnguyen96 <huongnguyenhd96@gmail.com>
2016-11-30 13:47:50 +0700

    update_gamebanruoi

--- CI1/js/main.js
+++ CI1/js/main.js
@@ -1,6 +1,9 @@
 var Nakama = {};
 Nakama.configs = {
-  SHIP_SPEED : 100//gán dl chung, khi thay đổi sẽ ko phải làm nhiều lần
+  SHIP_SPEED : 200,//gán dl chung, khi thay đổi sẽ ko phải làm nhiều lần
+  BACK_SPEED : 1,
+  FIRE_SPEED : 400,
+  FIRE_RATE : 60
 }
 window.onload = function(){
   Nakama.game = new Phaser.Game(
@@ -18,7 +21,10 @@ window.onload = function(){
     false
   );
 }
-
+var sprite;
+var weapon;
+var cursors;
+var fireButton;
 var preload = function(){
   Nakama.game.scale.minWidth = 320;
   Nakama.game.scale.minHeight = 480;
@@ -29,11 +35,17 @@ var preload = function(){
   Nakama.game.load.atlasJSONHash('assets', 'Assets/assets.png',
               'Assets/assets.json');
   Nakama.game.load.image('background', 'Assets/Map1.png');
+  Nakama.game.load.image('aliens','Assets/player.png');
   Nakama.game.time.advancedTiming = true;
-}
+};
+var starfield;
+var aliens;
 var create = function(){
+  //set background
+  Nakama.starfield = Nakama.game.add.tileSprite(0, 0, 640, 960, 'background');
   Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
   Nakama.keyboard  = Nakama.game.input.keyboard;
+  //set tau 1
   Nakama.ship2 = Nakama.game.add.sprite(
     200,
     400,
@@ -41,15 +53,57 @@ var create = function(){
     "Spaceship1-Partner.png"
   );
   Nakama.game.physics.enable(Nakama.ship2,Phaser.Physics.ARCADE);
-  Nakama.ship = Nakama.game.add.sprite(
-    200,
-    400,
-    'assets',
-    "Spaceship1-Player.png"
-  );
-  Nakama.game.physics.enable(Nakama.ship, Phaser.Physics.ARCADE);
+  //set tau 2
+  // Nakama.ship = Nakama.game.add.sprite(
+  //   200,
+  //   400,
+  //   'assets',
+  //   "Spaceship1-Player.png"
+  // );
+//  Nakama.game.physics.enable(Nakama.ship, Phaser.Physics.ARCADE);
+  //create bullet
+  Nakama.weapon = Nakama.game.add.weapon(30,'assets',"BulletType1.png");
+  Nakama.game.physics.enable(Nakama.bullet,Phaser.Physics.ARCADE);
+  Nakama.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+  Nakama.weapon.bulletAngleOffset = 90;
+  Nakama.weapon.bulletSpeed = Nakama.configs.FIRE_SPEED;
+  Nakama.weapon.fireRate = Nakama.configs.FIRE_RATE;
+  Nakama.weapon.trackSprite(Nakama.ship, 38,-12);
+  ///create Ailen
+  Nakama.ailens = Nakama.game.add.group();
+  aliens.enableBody = true;
+  aliens.physicsBodyType = Phaser.Physics.ARCADE;
+  createAliens();
+};
+function createAliens () {
+    for (var y = 0; y < 4; y++)
+    {
+        for (var x = 0; x < 10; x++)
+        {
+            var alien = aliens.create(x * 48, y * 50, 'ailens');
+            alien.anchor.setTo(0.5, 0.5);
+            alien.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
+            alien.play('fly');
+            alien.body.moves = false;
+        }
+    }
+
+    aliens.x = 100;
+    aliens.y = 50;
+
+    var tween = game.add.tween(aliens).to( { x: 200 },
+      2000,
+      Phaser.Easing.Linear.None, true, 0, 1000, true
+    );
+
+    //  When the tween loops it calls descend
+    tween.onLoop.add(descend, this);
 }
+
 var update = function() {
+    //  Scroll the background
+  Nakama.starfield.tilePosition.y += Nakama.configs.BACK_SPEED;
+  //tau 1
   if(Nakama.keyboard.isDown(Phaser.Keyboard.UP)){
     Nakama.ship.body.velocity.y = -Nakama.configs.SHIP_SPEED;
   }
@@ -69,7 +123,7 @@ var update = function() {
     Nakama.ship.body.velocity.X = 0;
   }
 //tau 2
-  if(Nakama.keyboard.isDown(Phaser.Keyboard.W)){
+/*  if(Nakama.keyboard.isDown(Phaser.Keyboard.W)){
     Nakama.ship2.body.velocity.y = -Nakama.configs.SHIP_SPEED;
   }
   else if(Nakama.keyboard.isDown(Phaser.Keyboard.S)){
@@ -87,5 +141,11 @@ var update = function() {
   else{
     Nakama.ship2.body.velocity.X = 0;
   }
+  */
+  //dan
+  if(Nakama.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+         Nakama.weapon.fire();
+     }
+
 }
 var render = function(){}
