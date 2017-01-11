3e7db534a7601819ab6df71022eb057248503044
huongnguyen96 <huongnguyenhd96@gmail.com>
2017-01-11 11:13:19 +0700

    BTVN_update3: icon fade/masory

--- TechkisdHotgirls/js/main.js
+++ TechkisdHotgirls/js/main.js
@@ -1,27 +1,71 @@
-/*$(document).ready(function(){
-  console.log('ready');
-  $('body').on('click','.overlay', function(){
-    alert("hello");
-  });
-  $('.overlay').on('click', function(){
-    alert("hello");
-  })
-});*/
+var loadedData=[];
+var isrequestNextPage = false;
 
 $(document).ready(function(){
   var itemTemplate = Handlebars.compile($("#item-template").html());
+  var itemModalTemplate = Handlebars.compile($("#item-modal-template").html());
+
   $.ajax({
     type : "get",
     url  : "/libs/bootstrap/js/imagesData.json"
   }).then(function(data){
-    var itemHtml = itemTemplate(data);
-    $("#item_list").html(itemHtml);
+    loadedData = loadedData.concat(data.items);
+
+    var $itemHtml = $(itemTemplate(data));
+    //$("#item_list").html(itemHtml);
+    $('#item_list').append($itemHtml).masonry('appended', $itemHtml, true);
     $('#item_list').masonry({
       itemSelector: '.item_container',
       columnWidth: '.item_container',
       percentPosition: true
     })
-  }).fail(function(data){
+  }).fail(function(error){
     console.log(error);
   });
+
+  $('body').on('click','.plus_button', function(){
+    var itemId = $(this).attr('data-item-id');
+    for(var i=0; i<loadedData.length;i++){
+      var itemData = loadedData[i];
+      if(itemData.id == itemId){
+        $('#item_modal_body').html(itemModalTemplate(itemData));
+        break;
+      }
+    }
+    $("#item_modal").modal("show");
+  });
+
+  $(window).on('scroll', function(){
+    if(isrequestNextPage) return;
+
+    if($(window).scrollTop()+window.innerHeight > $(document).height() - 200){
+      setTimeout(function() {
+        requestNextPage(itemTemplate);
+      },2000);
+      isrequestNextPage = true;
+    }
+  });
 });
+
+function requestNextPage(itemTemplate) {
+  $.ajax({
+    type : "get",
+    url  : "libs/bootstrap/js/imagesData.json"
+  }).then(function(data){
+    loadedData = loadedData.concat(data.items);
+
+    var $itemHtml = $(itemTemplate(data));
+    //them vao cuoi list item
+    $('#item_list').append($itemHtml).masonry('appended', $itemHtml, true);
+    $('#item_list').masory({
+      itemSelector: '.item_container',
+      columnWidth: '.item_container',
+      percentPosition: true
+    });
+  }).fail(function(error){
+    console.log(error);
+  }).always(function() {
+    isrequestNextPage = false;
+  });
+
+}
